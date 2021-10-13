54941855e392171de2afaef52e4ed4ec4c7a3b1c
Aarini0707 <79413058+Aarini0707@users.noreply.github.com>
2021-10-13 18:05:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 peter_pan = "";
 harry_potter = "";
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotposes);
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Left Wrist X= " + leftWristX + " Left Wrist Y= " + leftWristY);
+        console.log("Right Wrist X= " + rightWristX + " Right Wrist Y= " + rightWristY);
+    }
 }
